08a8bedc7a8906df13e86dc0fe44e188e6e06fba
Flávio <pereiracruz2002@gmail.com>
2024-10-11 17:36:13 -0300

    transformando em blade

---
+++ resources/views/anunciar.blade.php
@@ -0,0 +1,12 @@
+<x-layout>
+    <x-slot name="title">{{$title}}</x-slot>
+    <div class="container-fluid my-3">
+        <div class="row">
+            <div class="card mb-3">
+                <div class="row g-0">
+
+                </div>
+            </div>
+        </div>
+    </div>
+</x-layout>
---
+++ resources/views/blog.blade.php
@@ -0,0 +1,69 @@
+<x-layout>
+    <x-slot name="title">{{$title}}</x-slot>
+    <div class="container-fluid my-3">
+        <div class="row">
+            <div class="col-8">
+                <div class="card mb-3">
+                    <div class="row g-0">
+                        <div class="col-md-4">
+                        <img src="/img/groomer.jpg" class="img-fluid rounded-start" alt="...">
+                        </div>
+                        <div class="col-md-8">
+                        <div class="card-body">
+                            <h5 class="card-title">Empregos na area Pet</h5>
+                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                            <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+
+                <div class="card mb-3">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                    <img src="/img/profissionalPet.jpg" class="img-fluid rounded-start" alt="...">
+                    </div>
+                    <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">Como se destacar no mercado pet</h5>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+                    </div>
+                    </div>
+                </div>
+                </div>
+
+                <div class="card mb-3">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                    <img src="/img/Equipamentosd.jpeg" class="img-fluid rounded-start" alt="...">
+                    </div>
+                    <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">Equipamentos para profissionais</h5>
+                        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                        <p class="card-text"><small class="text-body-secondary">Last updated 3 mins ago</small></p>
+                    </div>
+                    </div>
+                </div>
+                </div>
+            </div>
+
+            <div class="col-4"> <!-- card lateral -->
+                <div class="card">
+                <img src="/img/img-banner-3.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+                </div>
+
+                <div class="card mt-3">
+                <img src="/img/img-banner-3.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+                </div>
+            </div>
+        </div>
+  </div>
+</x-layout>
